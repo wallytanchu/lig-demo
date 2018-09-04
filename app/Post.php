97843b4dbb8008134b33f8afeb570c44d8d89248
Wally Chu <wallychu@Wallys-MacBook-Pro.local>
2018-09-05 00:13:12 +0800

    Fix bug on updated model

--- app/Post.php
+++ app/Post.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-    protected $fillable = ['title', 'body', 'photo'];
+    protected $fillable = ['title', 'inquiry', 'photo'];
 }
