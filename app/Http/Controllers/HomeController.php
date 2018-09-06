f85b5d315afd8ec626440ef14f0ac3212f091b5f
Wally Chu <wallychu@Wallys-MacBook-Pro.local>
2018-09-06 21:08:42 +0800

    Implemented index page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts =  Post::orderby('created_at', 'desc')->orderby('id','desc')->paginate(5);
+        return view('index', ['posts' => $posts]);
+    }
+
+    /**
+     * View a post entry
+     * 
+     * @param Post $post
+     * @return 
+     */
+    public function single(Post $post)
+    {   
+        return view('single', ['post' => $post]); 
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+
+
+@extends('layouts.public')
+@section('content')
+    <!--start l-contents-->
+    <div class="l-container u-clear">
+
+        <!--start l-main-->
+        <main class="l-main js-main">
+            <div class="l-main-block"></div>
+            <div class="archive">
+                <ul class="archive-list">
+
+                @if($posts)
+                    @foreach($posts as $post)
+                        <li class="archive-item">
+                        <article class="card">
+                            <a href="/single/{{ $post->id }}" class="card-link">
+                                <img src="/{{ $post->photo }}" alt="" class="card-image">
+                                <div class="card-bottom">
+                                    <h1 class="card-title">{{ $post->title }}</h1>
+                                    <time class="card-date" datetime="{{ Carbon\Carbon::parse($post->created_at)->format('Y-n-j')  }}">
+                                        {{ Carbon\Carbon::parse($post->created_at)->format('d M, Y')  }}
+                                    </time>
+                                </div>
+                             </a>
+                        </article>
+                        </li>
+                    @endforeach
+                @endif
+                    
+                    
+                    
+                </ul>
+            </div>
+            <a href="/archive" class="archive-button">
+                <div class="button">
+    <p class="button-text">More</p>
+</div>
+            </a>
+        </main>
+        <!--end l-main-->
+
+    </div>
+    <!--end l-contents-->
+@endsection
